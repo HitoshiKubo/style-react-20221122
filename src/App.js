5704709f4cf4412bb62ac4d78a18bd70c18208b4
hitoshi.kubo <hitoshi.kubo3@gmail.com>
2022-11-22 17:53:27 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { InlineStyle } from "./components/InlineStyle.jsx";
+import { CssModules } from "./components/CssModules.jsx";
+import { StyledJsx } from "./components/StyledJsx.jsx";
+import { StyledComponents } from "./components/StyledComponents.jsx";
+import { Emotion } from "./components/Emotion.jsx";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
+    </div>
+  );
+}
